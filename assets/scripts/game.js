112e862bf13c00e158fb966e881e96814d566127
Eb0nY-April14 <laratemi14@gmail.com>
2021-09-07 15:23:00 +0000

    Add interactivity to the jest simon memory game

---
+++ assets/scripts/game.js
@@ -0,0 +1,112 @@
+let game = {    // This creates a new game object with a score property/attribute of zero
+    score: 0,
+    currentGame: [],
+    playerMoves: [],
+    turnNumber: 0,
+    choices: ["button1", "button2", "button3", "button4"],
+}
+
+/********************************************************************************************************************************/
+/*                                                     REMINDER:                                                               */
+/*      EVERYTIME WE ADD A NEW FUNCTION, WE NEED TO EXPORT IT FROM "game.js" FILE & IMPORT IT INTO "game.test.js" FILE.       */
+/*                                                                                                                           */
+/************************************************************************************************************************** */
+
+/* What the newGame() function does is that it clears out our data from the currentGame array. */
+function newGame() {
+    game.score = 0;     // This resets the score back to zero
+    game.playerMoves = [];   // This clears the playerMoves array
+    /* Using for-loop, we'll get all of the elements with a class of 'circle' & check the attribute of 
+    each circle. If the attribute is set to false, then we'll add our event listener by writing 
+    'circle.addEventListener'. */
+    for (let circle of document.getElementsByClassName("circle")) {
+        if (circle.getAttribute("data-listener") != "true") {
+            /* The code on line 25 will be a click event listener, we'll pass in the event object & the 
+            click object as 'e' since we'll use it to get our click targets ID so depending on the circle 
+            we clicked on, the ID will either be button1, button2, button3 or button4 & we'll store that 
+            ID in the 'move' variable. */
+            circle.addEventListener("click", (e) => {
+                let move = e.target.getAttribute("id"); 
+                lightsOn(move); // Here, we'll call our lightsOn() function using 'move' as the parameter & this will illuminate the correct circle.
+                game.playerMoves.push(move);    // This will push that 'move' into our game.playerMoves
+                playerTurn();   // this function is not written yet
+            });
+            circle.setAttribute("data-listener", "true");   // sets the data-listener attribute on our circle to true.
+        }
+    }
+    game.currentGame = [];   // This clears the currentGame array 
+    showScore();    // This will be called at the end of newGame 
+    addTurn();  // This will be called in our newGame() function straight after the showScore() function is called.   
+}
+
+/* What the addTurn() function does is that it pushes a random choice out. */
+function addTurn() {
+    game.playerMoves = [];   // This clears the playerMoves array bcos this is the start of a new turn
+    /* On the code on line 29 below, we'll use the Math.random() library to generate a random number 
+    between 0 & 3, that number will then be used as the index of our choices array, then the resulting 
+    choice is pushed onto the current game array. */
+    game.currentGame.push(game.choices[(Math.floor(Math.random() * 4))]);  
+    showTurns()
+}
+
+function showScore() {  // We'll export this function in 'game.js' file & import it into the 'game.test.js' file 
+    document.getElementById("score").innerText = game.score;
+}
+
+/* The lightsOn() function will take one parameter which is the ID of one of our circles 
+& inside this function, we'll refer to it as 'circ'. */ 
+function lightsOn(circ) {
+    /* On line 43, the class of light will be added to our appropriate circle by gettig 
+    the element with an ID of 'circle' that was passed into the function & then add the 
+    light class. */
+    document.getElementById(circ).classList.add("light");
+    /* We'll use the javaScript's setTimeout() function to remove this 'light' class 
+    from our 'classList' element after 400 millisecs. This is just a reversal of the 
+    code we wrote on line 43. */
+    setTimeout(() => {
+        document.getElementById(circ).classList.remove("light");
+    }, 400);
+}
+
+/* The showTurns() function sets the interval, calls the lightsOn() function to turn on the light, 
+increments the game turnNumber & then turn them off again i.e it will step through the currentGame 
+array & turn on the appropriate light then turn it off again. That is the computer sequence that the 
+player will try to follow. We are starting our global state in the game object so that's why we are 
+using it as a counter. */
+function showTurns() {
+    game.turnNumber = 0;
+    let turns = setInterval(() => {
+        /* Here, we call the lightsOn() function inside a JavaScript set interval. 
+        This is to ensure that we have a little pause between the lights been shown 
+        & the next step in the sequence. */
+        lightsOn(game.currentGame[game.turnNumber]);
+        game.turnNumber++;
+        /* Here, the sequence is finished & the interval is cleared if turnNumber is 
+        equal or over the length of our current game array. */
+        if (game.turnNumber >= game.currentGame.length) {
+            clearInterval(turns);
+        }
+    }, 800);
+}
+
+/*               */
+function playerTurn() {
+    let i = game.playerMoves.length - 1;    // This gets the index of the last element in playerMoves array.
+    /* What the code on line 97 does is that it compares the element at index i of currentGame array with that 
+    in the playerMoves array & if our player gets the answers correct, both should match.   */
+    if (game.currentGame[i] === game.playerMoves[i]) {
+        if (game.currentGame.length === game.playerMoves.length) {  // If both lengths are equal, it means we are at the end of the sequence & the player got them all correct.
+            game.score++;   // It increments the score
+            showScore();    // It displays the score
+            addTurn();  // calls the addTurn() function to add a new turn.
+        }
+    } else {  //If both lengths aren't equal, then display an alert that says "Wrong move!" as written on line 104
+        alert("Wrong move!");
+        newGame();
+    }
+}
+
+/* It is neccessary to put our game in curly braces bcos we'll be exporting more than one 
+object & function from this file. We must add any object & function created into this module.exports
+& also go back to our game.test.js file & import them into it or else our test won't work. */
+module.exports = { game, newGame, showScore, addTurn, lightsOn, showTurns, playerTurn };    
---
+++ assets/scripts/tests/game.test.js
@@ -0,0 +1,155 @@
+/**
+ * @jest-environment jsdom   // It is a must to include this piece of code at the top of our test file or else it will fail.
+ */
+
+//const { beforeAll, test, expect } = require("@jest/globals");
+
+/* We must export from the 'module.exports = { game, ... }' section of our 'game.js' file & import into 
+the 'const { game, ... } = require("../game")' section of our 'game.test.js' file before our test can work. */
+//const { test, expect } = require("@jest/globals");
+const { game, newGame, showScore, addTurn, lightsOn, showTurns, playerTurn } = require("../game");  
+
+/* We want to display an alert to the player when he makes a wrong move. To do that, we'll use jest to check 
+if an alert has been called by using Jest spy. This spy will wait & only report when it sees some interesting 
+activity. We'll set the spy up on line 15, the 1st argument to spy on is the 'window' & the 2nd is the name of 
+a method which in our case is "alert". Why we are doing this is bcos alert is a method of the window object 
+so when it happens, we'll catch & divert it harmlessly into an empty function. */
+jest.spyOn(window, "alert").mockImplementation(() => { });
+
+beforeAll(() => {
+    let fs = require("fs");
+    let fileContents = fs.readFileSync("index.html", "utf-8");
+    document.open();
+    document.write(fileContents);
+    document.close();
+});
+
+describe("game object contains correct keys", () => {
+    /* TEST 1: This tests if the game object in our 'game.js' file contains a key called score */
+    test("score key exists", () => {  
+        expect("score" in game).toBe(true); // this expects score to be a property in the game object in game.js file
+    });
+    /* TEST 2: This tests if the currentGame key (it's an array) which holds the sequence of computer moves exists 
+    in the game object in our 'game.js' file. */ 
+    test("currentGame key exists", () => {   
+        expect("currentGame" in game).toBe(true); // this expects currentGame to be a property in the game object in game.js file
+    });
+    /* TEST 3: This tests if the playerMoves key (it's an array) which holds the sequence of player moves exists 
+    in the game object in our 'game.js' file. */ 
+    test("playerMoves key exists", () => {   
+        expect("playerMoves" in game).toBe(true); // this expects playerMoves to be a property in the game object in game.js file
+    });
+    /* TEST 4: This tests if the choices key (it's an array) which holds the sequence of choices selected by a 
+    player exists in the game object in our 'game.js' file. */ 
+    test("choices key exists", () => {   
+        expect("choices" in game).toBe(true); // this expects choices to be a property in the game object in game.js file
+    });
+    /* TEST 5: This tests if the choices key (it's an array) contains the correct IDs i.e we are expecting our 
+    choices array to contain button1, button2, button3 & button4 IDs. */ 
+    test("choices contain correct ids", () => {   
+        expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]); // this expects choices to be a property in the game object in game.js file
+    });
+    /* TEST 6: This tests if the turnNumber key exists in the game object in our 'game.js' file. */ 
+    test("turnNumber key exists", () => {
+        expect("turnNumber" in game).toBe(true);  // this expects turnNumber to be a property in the game object in game.js file
+    });
+}); // end of 1st describe test
+
+describe("newGame works correctly", () => {
+    /* We'll use another 'beforeAll()' function bcos we want to set up the game state with some fake values to see 
+    if the newGame function resets them. */
+    beforeAll(() => {   //This runs before all of the tests are run.
+        game.score = 42;    // We set game.score equals to 42
+        game.playerMoves = ["button1", "button2"];  // We set game.playerMoves value equals to ["button1", "button2"] array     
+        game.currentGame = ["button1", "button2"]; // We set game.currentGame value i.e computer moves equals to ["button1", "button2"] array
+        /* The code on line 54 below will set the score on the DOM to 42 so we can see if it gets reset to 0 by newGame. */
+        document.getElementById("score").innerText = "42";  // The 42 should be in quotes as it's a string.
+        newGame();  // We call the newGame() function & when it's working, it will reset the score
+    });
+    /* TEST 1: Sets the game score to zero  */
+    test("should set game score to zero", () => {
+        expect(game.score).toEqual(0);
+    });
+    /* TEST 2a: Clears the currentGame i.e computer sequence array. Since a new function named addTurn() will be called 
+    at the end of the newGame() function, then this test 2a has to change from testing to see if the currentGame sequence 
+    is empty to testing if it contains one element which is the new turn that we've just added so we create a new test below 
+    named TEST 2b. */
+    /* test("should clear the computer sequence array", () => {
+        expect(game.currentGame.length).toEqual(0);     // We can use .toBe() here too
+        //expect(game.currentGame).toEqual([]);  // This code should work exactly as the one above it
+    }); */
+    /* TEST 2b:  Checks if the currentGame sequence i.e computer's game array contains one move.   */
+    test("should be one move in the computer's game array", () => {
+        expect(game.currentGame.length).toBe(1);
+    });
+    /* TEST 3: Clears the playerMoves array  */
+    test("should clear the player moves array", () => {
+        expect(game.playerMoves.length).toEqual(0);     // We can use .toBe() here too
+        //expect(game.playerMoves).toEqual([]); // This code should work exactly as the one above it
+    });
+    /* TEST 4: Resets the score  */
+    test("should display 0 for the element with an id of score", () => {
+        expect(document.getElementById("score").innerText).toEqual(0);     
+    });
+    /* TEST 5: Checks if data-listener attribute is set to true on each circle. */
+    test("expect data-listener to be true", () => {
+        /* What's happening on line 92 is that we'll get all the elements with a class of 'circle' 
+        & store them in a variable called 'elements' which is an array & is declared as a constant
+        to prevent any change to it.  */
+        const elements = document.getElementsByClassName("circle");
+        for (let element of elements) {
+            /* On line 95, we expect that the "data-listener" attribute of the selected element is set to true.  */
+            expect(element.getAttribute("data-listener")).toEqual("true");
+        }   
+    });
+});
+
+/* This describe block is testing the gameplay */
+describe("gameplay works correctly", () => {
+    beforeEach(() => {  // This runs before each test is run. This means that we will reset the state each time
+        game.score = 0; // score reset
+        game.currentGame = [];  // currentGame reset
+        game.playerMoves = [];  //playerMoves reset
+        addTurn();  // This will add a new turn to our currentGame() array so there should be +1 element(s) in the array now.
+    });
+    afterEach(() => {
+        game.score = 0;
+        game.currentGame = [];
+        game.playerMoves = [];
+    });
+    test("addTurn adds a new turn to the game", () => {
+        addTurn();
+        expect(game.currentGame.length).toBe(2);
+    });
+    test("should add correct class to light up the buttons", () => {
+        /* The code on line 121 below will get one of the IDs from currentGame array & assign it to a
+        variable called 'button'. We'll go for the 1st element in the currentGame array since we know 
+        that there will always be at least one element in it. */
+        let button = document.getElementById(game.currentGame[0]);
+        lightsOn(game.currentGame[0]);  // this calls the lightsOn() function with the same ID used on line 106 as its parameter
+        /* On line 124 below, we expect that our buttons class list should contain the light class. We used a new matcher 'toContain'. */ 
+        expect(button.classList).toContain("light");
+    });
+    /* This test changes/updates the value of turnNumber from 42 to 0  */
+    test("showTurns should update game.turnNumber", () => {
+        game.turnNumber = 42;
+        showTurns();    // When we call this function, it should reset the turnNumber
+        expect(game.turnNumber).toBe(0);
+    })
+    /* This test checks if the score increments if the turn/move is correct    */
+    test("should increment the score if the turn/move is correct", () => {
+        /* Here, we'll take the turn we added in our beforeEach() function of our gameplay & push it 
+        into the playerMoves array & then call the playerTurn() function on line 137. That is how we'll 
+        kno that we have a correct answer because the playerTurn & the computersTurn match each other */
+        game.playerMoves.push(game.currentGame[0]);
+        playerTurn();
+        expect(game.score).toBe(1);  //After calling the playerTurn() function on line 138, we expect the score to have increased.
+    })
+
+    test("should call an alert if the move is wrong", () => {
+        game.playerMoves.push("wrong"); // It'll push a string that says "wrong" into our playerMoves array instead of pushing the correct move.
+        playerTurn();
+        /* From the code on line 153 below, We are expecting an alert box to be called/displayed with the text "Wrong move!" */
+        expect(window.alert).toBeCalledWith("Wrong move!");  // Since we are spying on our alert function, we'll see when it's called & what parameters it's called with. 
+    })
+});
